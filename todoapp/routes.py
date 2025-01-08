c5c89ca0528a6b86fbe55446187e543b461c17fe
unknown <aryansingla45@gmail.com>
2025-01-09 04:14:39 +0530

    login page - epe , epr

--- todoapp/routes.py
+++ todoapp/routes.py
@@ -3,12 +3,27 @@ from flask import render_template , flash , redirect , url_for , request
 from todoapp.forms import LoginForm , Registerform
 from todoapp.models import User , Employer, Employee
 from flask_login import login_user , current_user , logout_user , login_required
+from todoapp.utils import get_user_dashboard
 
 
 @app.route('/')
 def home():
     if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
+        user_id = current_user.id
+        
+        # Check if the user is an Employer
+        employer = Employer.query.filter_by(id=user_id).first()
+        if employer:
+            return redirect(url_for('dashboard_employer'))
+
+        # Check if the user is an Employee
+        employee = Employee.query.filter_by(id=user_id).first()
+        if employee:
+            return redirect(url_for('dashboard_employee'))
+        
+        # If neither is found (error handling)
+        flash('User role is undefined or invalid.', 'danger')
+        return redirect(url_for('login'))
     return render_template('index.html')
 
 @app.route('/about')
@@ -17,28 +32,32 @@ def about():
 
 
 
-@app.route('/login' , methods = ['GET' , 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
+        user_id = current_user.id
+        return get_user_dashboard(user_id)
+
     form = LoginForm()
+
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password , form.password.data):
-            login_user(user , remember = form.remember.data)
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+
             if next_page:
                 return redirect(next_page)
             else:
-                email = form.email.data
-                query_result = Employee.query.filter_by(email = email).first()
-                if query_result:
-                    return redirect(url_for('dashboard_employee'))
-                else:
-                    return redirect(url_for('dashboard_employer'))
+                user_id = user.id
+                return get_user_dashboard(user_id)
+
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html' , form = form)
+
+    return render_template('login.html', form=form)
+
 
 
 
@@ -51,7 +70,8 @@ def register():
     form = Registerform()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username = form.username.data , email = form.email.data , password = hashed_password , role = form.role.data , reference_id = form.reference_id.data)
+        user = User(username = form.username.data , email = form.email.data , password = hashed_password , 
+                    role = form.role.data , reference_id = form.reference_id.data)
         db.session.add(user)
         db.session.commit()
 
@@ -101,6 +121,7 @@ def dashboard_employee():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('home'))
