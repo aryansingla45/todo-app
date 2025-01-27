0d10db005fe2c738110d585424a91f7a802f4122
Aryan Singla <aryansingla45@gmail.com>
2025-01-27 19:28:25 +0530

    Added Readme.MD

---
+++ README.md
@@ -0,0 +1,7 @@
+# To-Do APP 
+
+- This app is powered by Python's Flask Framework. 
+- It implemtes the CRUD functionality for roles like employers and employee.
+- It uses the Flask-Login for user authentication and bcrypt for hashing the passwords.
+- It also has flask-mail support which helps the user to recreate passwords, if forgotten.
+- Modular App Structure.
--- todoapp/app_init.py
+++ todoapp/app_init.py
@@ -1,24 +1,30 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4d8s1127df7df484d8fsd484d8f4'
 app.config['SALT'] = '4d8s1127df7df484d45s18s81ds77778a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/todoapp_database'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 loginmanager = LoginManager(app)
 loginmanager.login_view = 'auth.login'
 
+migrate = Migrate(app, db)
+
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'todo.webapp6@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ytss arbn mjvv qaji'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+print(app.config['MAIL_PASSWORD'])
 
 mail = Mail(app)
 
